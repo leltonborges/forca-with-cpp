83d1be380848e47c51a0076918c06eca23c7821f
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-01-29 12:27:26 -0300

    refactory: main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include "forca.h"
-#include "demo.h"
-
-void foo();
 
 int main() {
     play();
 
     return 0;
-}
-
-void foo() {
-    showWord(secret_word, false);
-    showWord(guessHits, false);
 }
